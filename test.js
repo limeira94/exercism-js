cf3ef3a1e0c457e23c8c049ac67f80d3eee440fd
Felipe Limeira <limeira.felipe94@gmail.com>
2024-07-14 15:59:37 -0300

    add ex 14

--- test.js
+++ test.js
@@ -1,1 +1,20 @@
-console.log("test")
+function ticketStatus(tickets, ticketId) {
+
+    let message = '';
+
+    if (tickets[ticketId] === undefined) {
+        message += 'unknown ticket id';
+    } else if (tickets[ticketId] === null) {
+        message += 'not sold';
+    } else {
+        message += `sold to ${tickets.ticketId}`;
+    };
+    return message;
+};
+
+
+const tickets = {
+    '0H2AZ123': null,
+    '23LA9T41': 'Verena Nardi',
+};
+console.log(ticketStatus(tickets, '0H2AZ123'));
